4a79c0a1720fb4f97bc390c8bb0472334ddc8aae
bgailleton <boris.gailleton@gmail.com>
2019-06-24 14:10:50 +0100

    Refreshing and updating the code

--- basic_histogram.py
+++ basic_histogram.py
@@ -1,6 +1,8 @@
+# These two lines deactivate GUI from matplotlib.
+# It disable plt.show() so uncomment if you plan to use it
+# However some settings does not support GUI and won't let you import matplotlib if these lines are not there
 import matplotlib
 matplotlib.use("Agg")
-## Uncomment the two first lines if you are working on the server
 from matplotlib import pyplot as plt
 import pandas as bamboo_bears
 
@@ -18,11 +20,14 @@ fig = plt.figure(figsize = (3.5,2.7), facecolor = "white")
 gs = plt.GridSpec(100,100,bottom=0.15,left=0.10,right=0.90,top=0.95)
 ax = fig.add_subplot(gs[0:100,0:100], facecolor = "None")
 
-# Plotting whatever
-ax.hist(df["delta_ksn"],bins = 80)
+# Plotting whatever, all the options are explained there -> https://matplotlib.org/3.1.0/api/_as_gen/matplotlib.pyplot.hist.html
+## Here I used common parameters to custoise:
+## bins: number of bins // facecolor: the color of the bars // edgecolor: the color of the contouring lines
+## lw: the width of contour lines // density: normalised to 1 if True // histtype: define the style, here: vertical bars with only the external contours
+ax.hist(df["delta_ksn"],bins = 80, facecolor = "gray", edgecolor = "k", lw = 0.5, density = True, histtype = "stepfilled")
 
 # Changing the axis titles
-ax.set_xlabel(r"$\Delta k_{sn}$")
+ax.set_xlabel(r"$\Delta k_{sn}$") # r+ $$ tells matplotlib to use LaTex style compiler for mathematical expression
 ax.set_ylabel("Frequency")
 
 # Changing the axis ticks location
